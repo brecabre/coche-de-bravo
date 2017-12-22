7a2a3616753c5aa8bff865402c4fb8565ca0b540
brecabre <bravomantonio@gmail.com>
2017-12-22 18:39:44 +0100

    Add files via upload

--- GLOBAL.gd
+++ GLOBAL.gd
@@ -1,5 +1,6 @@
 extends Node
-var posiciones_coche =[]
+
+#posiciones coche
 
 var	posicion_coche_x_0 = []
 var	posicion_coche_x_1 = []
@@ -13,7 +14,63 @@ var	posicion_coche_z_0 = []
 var	posicion_coche_z_1 = []
 var	posicion_coche_z_2 = []
 
+var origen_coche_0 = []
+var origen_coche_1 = []
+var origen_coche_2 = []
 
+### posiciones circuito####
+
+var	posicion_fantasma_circuito_1_x_0 = []
+var	posicion_fantasma_circuito_1_x_1 = []
+var	posicion_fantasma_circuito_1_x_2 = []
+	
+var	posicion_fantasma_circuito_1_y_0 = []
+var	posicion_fantasma_circuito_1_y_1 = []
+var	posicion_fantasma_circuito_1_y_2 = []
+	
+var	posicion_fantasma_circuito_1_z_0 = []
+var	posicion_fantasma_circuito_1_z_1 = []
+var	posicion_fantasma_circuito_1_z_2 = []
+
+var origen_fantasma_circuito_1_0 = []
+var origen_fantasma_circuito_1_1 = []
+var origen_fantasma_circuito_1_2 = []
+
+var posiciones_coche = {
+	"posicion_coche_x_0":posicion_coche_x_0,
+	"posicion_coche_x_1":posicion_coche_x_1,
+	"posicion_coche_x_2":posicion_coche_x_2,
+	
+	"posicion_coche_y_0":posicion_coche_y_0,
+	"posicion_coche_y_1":posicion_coche_y_1,
+	"posicion_coche_y_2":posicion_coche_y_2,
+	
+	"posicion_coche_z_0":posicion_coche_z_0,
+	"posicion_coche_z_1":posicion_coche_z_1,
+	"posicion_coche_z_2":posicion_coche_z_2,
+	
+	"origen_coche_0": origen_coche_0,
+	"origen_coche_1": origen_coche_1,
+	"origen_coche_2": origen_coche_2
+}
+var posiciones_fantasma_circuito_1 = {
+
+	"posicion_fantasma_circuito_1_x_0":posicion_fantasma_circuito_1_x_0,
+	"posicion_fantasma_circuito_1_x_1":posicion_fantasma_circuito_1_x_1,
+	"posicion_fantasma_circuito_1_x_2":posicion_fantasma_circuito_1_x_2,
+	
+	"posicion_fantasma_circuito_1_y_0":posicion_fantasma_circuito_1_y_0,
+	"posicion_fantasma_circuito_1_y_1":posicion_fantasma_circuito_1_y_1,
+	"posicion_fantasma_circuito_1_y_2":posicion_fantasma_circuito_1_y_2,
+	
+	"posicion_fantasma_circuito_1_z_0":posicion_fantasma_circuito_1_z_0,
+	"posicion_fantasma_circuito_1_z_1":posicion_fantasma_circuito_1_z_1,
+	"posicion_fantasma_circuito_1_z_2":posicion_fantasma_circuito_1_z_2,
+	
+	"origen_fantasma_circuito_1_0": origen_fantasma_circuito_1_0,
+	"origen_fantasma_circuito_1_1": origen_fantasma_circuito_1_1,
+	"origen_fantasma_circuito_1_2": origen_fantasma_circuito_1_2
+}
 # graba los tiempos del coche y del fantasma con la longitud de las listas
 #var tiempo_coche = posicion_coche.size()
 #var tiempo_fantasma = posicion_fantasma.size()
@@ -28,31 +85,38 @@ var direccion = "res://fantasma.json"
 func save_game():	
 	
 	var salvar = {
-	"posicion_coche_x_0":posicion_coche_x_0,
-	"posicion_coche_x_1":posicion_coche_x_1,
-	"posicion_coche_x_2":posicion_coche_x_2,
+	"posiciones_fantasma_circuito_1": posiciones_fantasma_circuito_1
 	
-	"posicion_coche_y_0":posicion_coche_y_0,
-	"posicion_coche_y_1":posicion_coche_y_1,
-	"posicion_coche_y_2":posicion_coche_y_2,
+	}
 	
-	"posicion_coche_z_0":posicion_coche_z_0,
-	"posicion_coche_z_1":posicion_coche_z_1,
-	"posicion_coche_z_2":posicion_coche_z_2
-	}	
 	var file = File.new()
 	file.open(direccion, file.WRITE)
 	file.store_line(salvar.to_json())
 	file.close()	
 	
-#func load_game():  
-#	
-#	var file = File.new()
-#	if not file.file_exists(direccion):
-#		return
-#	
-#	file.open(direccion, File.READ)
-#	var data = {}
-#	data.parse_json(file.get_as_text())
-#	diccionario = data.diccionario
+func load_game():  
+	
+	var file = File.new()
+	if not file.file_exists(direccion):
+		return
+	
+	file.open(direccion, File.READ)
+	var data = {}
+	data.parse_json(file.get_as_text())
+	
+	posicion_fantasma_circuito_1_x_0 = data.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_x_0"]
+	posicion_fantasma_circuito_1_x_1 = data.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_x_1"]
+	posicion_fantasma_circuito_1_x_2 = data.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_x_2"]
+	
+	posicion_fantasma_circuito_1_y_0 = data.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_y_0"]
+	posicion_fantasma_circuito_1_y_1 = data.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_y_1"]
+	posicion_fantasma_circuito_1_y_2 = data.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_y_2"]
+	
+	posicion_fantasma_circuito_1_z_0 = data.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_z_0"]
+	posicion_fantasma_circuito_1_z_1 = data.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_z_1"]
+	posicion_fantasma_circuito_1_z_2 = data.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_z_2"]
+
+	origen_fantasma_circuito_1_0 = data.posiciones_fantasma_circuito_1["origen_fantasma_circuito_1_0"]
+	origen_fantasma_circuito_1_1 = data.posiciones_fantasma_circuito_1["origen_fantasma_circuito_1_1"]
+	origen_fantasma_circuito_1_2 = data.posiciones_fantasma_circuito_1["origen_fantasma_circuito_1_2"]
 	
--- main.gd
+++ main.gd
@@ -1,55 +1,103 @@
 extends Spatial
 
 
-onready var fantasma_escena = load("res://fantasma/fantasma.tscn")
+onready var fantasma_escena = preload("res://fantasma/fantasma.tscn")
 var numero_frame = 0
-var cargar_fantasma = false
+var grabar_coche = true
 var fantasma_corre = false
 
+
 func _ready():
-#	GLOBAL.load_game()
+	cargar_fantasma()
 	set_process_input(true)
 	set_fixed_process(true)
-	pass
 
+### carga el diccionario y instancia la escena del fantasma ###
 func cargar_fantasma():
-		var fantasma = fantasma_escena.instance()
-		get_parent().add_child(fantasma)
-		print("hollllalala",get_global_transform().translated())
-#		fantasma.set_transform(0,0,0)
-#		fantasma.set_transform(Vector3(0,0,0)).basis.x
-#		fantasma.set_transform(
-#		[GLOBAL.posicion_coche_x_0[numero_frame] , GLOBAL.posicion_coche_x_1[numero_frame] , GLOBAL.posicion_coche_x_2[numero_frame] ],
-#		[GLOBAL.posicion_coche_y_0[numero_frame] , GLOBAL.posicion_coche_y_1[numero_frame] , GLOBAL.posicion_coche_y_2[numero_frame] ],
-#		[GLOBAL.posicion_coche_z_0[numero_frame] , GLOBAL.posicion_coche_z_1[numero_frame] , GLOBAL.posicion_coche_z_2[numero_frame] ]
-#		).basis
-		fantasma.set_global_transform(GLOBAL.posiciones_coche[numero_frame])
-#		
+	GLOBAL.load_game()
+	var fantasma = fantasma_escena.instance()
+	get_parent().add_child(fantasma)
 
+### hace correr al fantasma recorriendo las listas del diccionario con "numero_frame" ###
 func fantasma_corre():
-	if 	fantasma_corre:
-		numero_frame +=1
-		get_tree().get_root().get_node("fantasma").set_global_transform(GLOBAL.posiciones_coche[numero_frame])
-#		if numero_frame >= GLOBAL.posicion_coche_x.size()-1:
-#			pass
-#		else: numero_frame +=1
+	var nodo_fantasma = get_tree().get_root().get_node("fantasma")
+	
+	if GLOBAL.posiciones_fantasma_circuito_1.empty():
+		pass	
+	else:
+		nodo_fantasma.set_global_transform(Transform(
 		
+		Vector3(GLOBAL.posicion_fantasma_circuito_1_x_0[numero_frame] , GLOBAL.posicion_fantasma_circuito_1_x_1[numero_frame] , GLOBAL.posicion_fantasma_circuito_1_x_2[numero_frame]),
+		Vector3(GLOBAL.posicion_fantasma_circuito_1_y_0[numero_frame] , GLOBAL.posicion_fantasma_circuito_1_y_1[numero_frame] , GLOBAL.posicion_fantasma_circuito_1_y_2[numero_frame]),
+		Vector3(GLOBAL.posicion_fantasma_circuito_1_z_0[numero_frame] , GLOBAL.posicion_fantasma_circuito_1_z_1[numero_frame] , GLOBAL.posicion_fantasma_circuito_1_z_2[numero_frame]),
+		Vector3(GLOBAL.origen_fantasma_circuito_1_0[numero_frame],GLOBAL.origen_fantasma_circuito_1_1[numero_frame],GLOBAL.origen_fantasma_circuito_1_2[numero_frame])))
+		
+		if numero_frame >= GLOBAL.posicion_fantasma_circuito_1_x_0.size()-1:
+			pass
+		else: numero_frame +=1
+
+func grabar_coche():	
+	var coche_nodo = get_node("coche/vehicle")
+	
+	GLOBAL.posicion_coche_x_0.append(coche_nodo.get_global_transform().basis.x[0])
+	GLOBAL.posicion_coche_x_1.append(coche_nodo.get_global_transform().basis.x[1])
+	GLOBAL.posicion_coche_x_2.append(coche_nodo.get_global_transform().basis.x[2])
+	
+	GLOBAL.posicion_coche_y_0.append(coche_nodo.get_global_transform().basis.y[0])
+	GLOBAL.posicion_coche_y_1.append(coche_nodo.get_global_transform().basis.y[1])
+	GLOBAL.posicion_coche_y_2.append(coche_nodo.get_global_transform().basis.y[2])
+	
+	GLOBAL.posicion_coche_z_0.append(coche_nodo.get_global_transform().basis.z[0])
+	GLOBAL.posicion_coche_z_1.append(coche_nodo.get_global_transform().basis.z[1])
+	GLOBAL.posicion_coche_z_2.append(coche_nodo.get_global_transform().basis.z[2])
+	
+	GLOBAL.origen_coche_0.append(coche_nodo.get_global_transform().origin[0])
+	GLOBAL.origen_coche_1.append(coche_nodo.get_global_transform().origin[1])
+	GLOBAL.origen_coche_2.append(coche_nodo.get_global_transform().origin[2])
+	
 func _input(event):
+	
 	if (Input.is_action_pressed("aparece_fantasma")):	
 		cargar_fantasma()
 		fantasma_corre = true
-		
+	
+	if (Input.is_action_pressed("salvar")):
+		GLOBAL.save_game()	
+
 func _fixed_process(delta):
+	if grabar_coche:
+		grabar_coche()
 	if fantasma_corre:
 		fantasma_corre()
 
-# area que hace que deje de grabar las posiciones del coche
-#func _on_Area_body_enter( body ):	
-# deja de grabar y sustituye el tiempo del fantasma por el del coche si este es menor 
-#	GLOBAL.grabar_fantasma = false
+# area que hace que deje de grabar las posiciones del coche  "meta"
+func _on_meta_body_enter( body ):
+ #deja de grabar y sustituye el tiempo del fantasma por el del coche si este es menor 
+	grabar_coche = false
+#	
+	if GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_x_0"].empty() or GLOBAL.posiciones_coche["posicion_coche_x_0"].size() < GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_x_0"].size():
+		
+		GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_x_0"] = GLOBAL.posicion_coche_x_0
+		GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_x_1"] = GLOBAL.posicion_coche_x_1
+		GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_x_2"] = GLOBAL.posicion_coche_x_2
 	
-#	if GLOBAL.posicion_fantasma.empty() or GLOBAL.posicion_coche.size() < GLOBAL.posicion_fantasma.size():
+		GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_y_0"] = GLOBAL.posicion_coche_y_0
+		GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_y_1"] = GLOBAL.posicion_coche_y_1
+		GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_y_2"] = GLOBAL.posicion_coche_y_2
 		
-#		GLOBAL.posicion_fantasma = GLOBAL.posicion_coche
-#		GLOBAL.save_game()
+		GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_z_0"] = GLOBAL.posicion_coche_z_0
+		GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_z_1"] = GLOBAL.posicion_coche_z_1
+		GLOBAL.posiciones_fantasma_circuito_1["posicion_fantasma_circuito_1_z_2"] = GLOBAL.posicion_coche_z_2
 		
+		GLOBAL.posiciones_fantasma_circuito_1["origen_fantasma_circuito_1_0"] = GLOBAL.origen_coche_0
+		GLOBAL.posiciones_fantasma_circuito_1["origen_fantasma_circuito_1_1"] = GLOBAL.origen_coche_1
+		GLOBAL.posiciones_fantasma_circuito_1["origen_fantasma_circuito_1_2"] = GLOBAL.origen_coche_2
+		
+		GLOBAL.save_game()
+		print("entro")
+
+
+
+
+
+	
